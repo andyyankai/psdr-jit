6e80819e5c478f2cb7941dae13d0067b5ea5bdb3
Kai Yan <kyan8@uci.edu>
2022-09-17 12:43:29 +0800

    add mutipass grad example

--- unit_test/inv_test.py
+++ unit_test/inv_test.py
@@ -13,7 +13,7 @@ output_path.mkdir(parents=True, exist_ok=True)
 
 sc = psdr.Scene()
 sc.load_file("bunny_env.xml")
-sc.opts.spp = 32
+sc.opts.spp = 128
 sc.opts.sppe = 0
 sc.opts.sppse = 0
 sc.opts.sppse = 0
@@ -44,20 +44,36 @@ output = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 cv2.imwrite("inv_init.exr", output)
 
 
+sc.opts.spp = 32
+sc.opts.sppe = 0
+sc.opts.sppse = 0
+sc.opts.sppse = 0
+sc.opts.log_level = 0
+
+loop_spp = 2
 for it in range(0, 100):
 	init_diffuse = Vector3fD(detach(init_diffuse))
 	drjit.enable_grad(init_diffuse)
 	sc.param_map['BSDF[0]'].reflectance.data = init_diffuse;
 	sc.configure()
-	img = integrator.renderD(sc, 0)
-	loss = mean(mean(abs(img_target - img)))
-	backward(loss)
+	I = Vector3fD(integrator.renderC(sc, 0))
+	drjit.enable_grad(I)
+	I0 = img_target
+	L = mean(mean(abs(I0 - I)))
+	backward(L)
+	dL = grad(I)
+	for ii in range(0, loop_spp):
+		img_ad = integrator.renderD(sc, 0)
+		tmp = dot(dL/float(loop_spp), img_ad)
+		backward(tmp)
 	texture_g = grad(init_diffuse)
 	init_diffuse = init_diffuse-texture_g
-	print("iter", it, loss, init_diffuse)
-
-	# img = img.numpy().reshape((sc.opts.width, sc.opts.height, 3))
+	print("iter", it, L, init_diffuse)
+	# img = I.numpy().reshape((sc.opts.width, sc.opts.height, 3))
 	# output = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
 	# cv2.imwrite(str(output_path)+"/inv_iter"+str(it)+".exr", output)
+	del texture_g, img_ad, I, tmp, L
+	drjit.registry_trim()
+
 
 
