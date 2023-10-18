e775947f2bdb18d21a9d8855bfac7b7c33f18d66
GuangyanCai <gcai3@uci.edu>
2023-10-19 00:47:21 +0800

    Added a simple stop watch for benchmarking.

---
+++ include/misc/utils.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <chrono>
+#include <string>
+#include <iostream>
+#include <iomanip>
+
+class StopWatch {
+public:
+    StopWatch() : is_running(false) {}
+
+    void start(std::string name) {
+        if (!is_running) {
+            m_name = name;
+            start_time = std::chrono::high_resolution_clock::now();
+            is_running = true;
+        }
+    }
+
+    void stop() {
+        if (is_running) {
+            end_time = std::chrono::high_resolution_clock::now();
+            double elapsed = std::chrono::duration<double>(end_time - start_time).count();
+            std::cout << std::fixed << std::setprecision(3) << "[" << m_name << "] " << "Elapsed time: " << elapsed  << " s" << std::endl;
+
+            // Reset the stopwatch
+            is_running = false;
+            start_time = std::chrono::high_resolution_clock::time_point();
+            end_time = std::chrono::high_resolution_clock::time_point();
+        }
+    }
+
+private:
+    bool is_running;
+    std::string m_name;
+    std::chrono::high_resolution_clock::time_point start_time;
+    std::chrono::high_resolution_clock::time_point end_time;
+};
