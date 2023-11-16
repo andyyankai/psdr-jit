f2c12d54bb279da8de872b952cf9faef0cb8fb85
Kai Yan <kyan8@uci.edu>
2023-11-16 13:39:19 +1300

    minor fix

--- src/psdr.cpp
+++ src/psdr.cpp
@@ -431,9 +431,9 @@ PYBIND11_MODULE(psdr_jit, m) {
         .def("preprocess_secondary_edges", &PathTracer::preprocess_secondary_edges, "scene"_a, "sensor_id"_a, "resolution"_a, "nrounds"_a = 1)
         .def_readwrite("hide_emitters", &PathTracer::m_hide_emitters);
 
-    py::class_<PathTracer, Integrator>(m, "Direct")
+    py::class_<DirectIntegrator, Integrator>(m, "Direct")
         .def(py::init<int>(), "mis"_a = 2)
-        .def("preprocess_secondary_edges", &PathTracer::preprocess_secondary_edges, "scene"_a, "sensor_id"_a, "resolution"_a, "nrounds"_a = 1)
-        .def_readwrite("hide_emitters", &PathTracer::m_hide_emitters);
+        .def("preprocess_secondary_edges", &DirectIntegrator::preprocess_secondary_edges, "scene"_a, "sensor_id"_a, "resolution"_a, "nrounds"_a = 1)
+        .def_readwrite("hide_emitters", &DirectIntegrator::m_hide_emitters);
 
 }
